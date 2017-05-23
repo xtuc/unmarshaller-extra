e0147ed8f88362632b7ddb9f5abe8ac5a8bce431
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-05-23 22:09:24 +0200

    feat: knobs generation

---
+++ packages/unmarshaller-generator-storybook-knobs/src/index.js
@@ -0,0 +1,59 @@
+import * as knobs from '@kadira/storybook-addon-knobs';
+
+/**
+ * Generate knobs from unmarshaller
+ *
+ * @param {Object} unmarshaller
+ * @returns {Object}
+ */
+export function generateKnobs(unmarshaller: Object) {
+  const keys = Object.keys(unmarshaller);
+
+  return keys.reduce((acc, key) => {
+    let {name, type, defaultValue, exampleValue, children, of} = unmarshaller[key];
+    let res;
+
+    if (type === 'holder') {
+      acc[key] = generateKnobs(children);
+      return acc;
+    }
+
+    defaultValue = defaultValue !== null && defaultValue !== undefined ? defaultValue : exampleValue;
+
+    console.log(knobs);
+
+    switch(type) {
+    case 'string':
+    case 'icon':
+      if (of) {
+        res = knobs.select(name, of, defaultValue);
+      } else {
+        res = knobs.text(name, defaultValue || '');
+      }
+      break;
+
+    case 'boolean':
+      res = knobs.boolean(name, defaultValue || false);
+      break;
+
+    case 'color':
+      res = knobs.color(name, defaultValue || 'white');
+      break;
+
+    case 'number':
+      res = knobs.number(name, defaultValue);
+      break;
+
+    case 'object':
+      res = knobs.object(name, defaultValue || {});
+      break;
+
+    default:
+      res = knobs.text(name, '');
+    }
+
+    acc[key] = res;
+
+    return acc;
+  }, {});
+}
